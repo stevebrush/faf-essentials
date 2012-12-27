c493078639241732229183a089ad1426799802a3
Steve Brush <stevebrush@aol.com>
2012-12-27 15:20:50 -0500

    Thermometer Patch

---
+++ patch/therm.js
@@ -0,0 +1,16 @@
+Patch = {
+	formatCurrency: function(num) {
+		var num = num.toString().replace(/\$|\,/g,'');
+		for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+			num = num.substring(0,num.length-(4*i+3))+','+num.substring(num.length-(4*i+3));
+		return ((('$')?'':'-') + '$' + num);
+	},
+	therm: function() {
+		var goalContainer = $('#fundbar').find('td.fundbarGoal div.fundValue');
+		var goalValue = goalContainer.attr('amount');
+		goalContainer.html(Patch.formatCurrency(goalValue));
+	}
+};
+$(document).ready(function(){
+Patch.therm();
+});
